d9dfa460471927c130ab82c268f50ff372ab40ea
diggy <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-09-04 15:34:56 +0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,37 +8,49 @@ from selenium.webdriver.common.by import By
     
 def scrape_tenders(url):
 
+# setting up chrome driver options
     chrome_driver_path = "./chromedriver.exe"
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
+    
+#scraping tenders data from the tenders page
     try:
         driver.get(url)
         time.sleep(10)
+    # finding and clicking the tenders button
         tenders_button = driver.find_element(By.LINK_TEXT, "Tenders")
         tenders_button.click()
         time.sleep(5)
-        all_tenders_button = driver.find_element(By.LINK_TEXT, "ALL TENDERS")
-        all_tenders_button.click()
-        time.sleep(5)
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        print(soup)
+    # finding the tenders table
+        table = driver.find_element(By.TAG_NAME, "table")
+        tenders = []
+        count = 0
         
+# looping through the tenders table rows
+        for row in table.find_elements(By.XPATH, ".//tbody/tr"):
+            if count >= limit:
+                break
+        # finding the tables's cells
+            cells = row.find_elements(By.TAG_NAME, "td")
+            
+        # stripping the the required cells
+            if len(cells) > 0:
+                tender_no = cells[0].text.strip()
+                description = cells[1].text.strip()
+                procuring_entity = cells[2].text.strip()
+                closing_date = cells[5].text.strip()
+
+        # adding the stripped tenders data to our empty tender list
+                tenders.append({
+                    'Tender Number': tender_no,
+                    'Description': description,
+                    'Procuring Entity': procuring_entity,
+                    'Closing Entity': closing_date
+                    })
+                count +=1
+
+        return tenders
+#stopping the automation    
     finally:
         driver.quit()
-
-def clean_content(content):
-    soup = BeautifulSoup(content, "html.parser")
-
-    for remove_ss in soup(["scrypt"], ["style"]):
-        remove_ss.extract()
-
-    valid_content = soup.get_text(separator = "\n")
-    valid_content = "\n".join(line.strip() for line in valid_content.splitlines() if line.strip())
-    
-
-    return valid_content
-
-tenders = []
 
