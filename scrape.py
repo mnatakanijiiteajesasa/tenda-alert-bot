18c4fc112a15f7d760232da904cb387ba66bde06
diggy <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-09-10 11:07:30 +0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,56 +1,73 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import datetime
 import time
 from selenium.webdriver.common.by import By
 
-    
-def scrape_tenders(url):
 
-# setting up chrome driver options
+def scrape_tenders(url, limit):
+
+# setting up chrome options
     chrome_driver_path = "./chromedriver.exe"
     options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-    
-#scraping tenders data from the tenders page
+
     try:
         driver.get(url)
         time.sleep(10)
-    # finding and clicking the tenders button
         tenders_button = driver.find_element(By.LINK_TEXT, "Tenders")
         tenders_button.click()
         time.sleep(5)
-    # finding the tenders table
-        table = driver.find_element(By.TAG_NAME, "table")
-        tenders = []
-        count = 0
-        
-# looping through the tenders table rows
-        for row in table.find_elements(By.XPATH, ".//tbody/tr"):
-            if count >= limit:
-                break
-        # finding the tables's cells
-            cells = row.find_elements(By.TAG_NAME, "td")
-            
-        # stripping the the required cells
-            if len(cells) > 0:
-                tender_no = cells[0].text.strip()
-                description = cells[1].text.strip()
-                procuring_entity = cells[2].text.strip()
-                closing_date = cells[5].text.strip()
+       
 
-        # adding the stripped tenders data to our empty tender list
+        tenders = []
+        current_page = 1
+        while True:
+            table = driver.find_element(By.TAG_NAME, "table")
+# looping through the tenders table rows
+            for row in table.find_elements(By.XPATH, ".//tbody/tr"):
+                cells = row.find_elements(By.TAG_NAME, "td")
+                if len(cells) > 0:
+                    tender_no = cells[0].text.strip()
+                    description = cells[1].text.strip()
+                    procuring_entity = cells[2].text.strip()
+                    closing_date = cells[5].text.strip()
+
+            # if customer_tender.lower() in description.lower():
                 tenders.append({
                     'Tender Number': tender_no,
                     'Description': description,
                     'Procuring Entity': procuring_entity,
-                    'Closing Entity': closing_date
+                    'Closing Date': closing_date
                     })
-                count +=1
+             
+
+            next_page_button = driver.find_element(By.XPATH, "//li[@class='v-pagination__next']/button")
+
+            is_disabled = next_page_button.get_attribute("aria-disabled")
+
+            if is_disabled == 'true':
+                print("No more pages to scrape")
+                break
+            else:
+                next_page_button.click()
+                time.sleep(3)
+
+            if limit and len(tenders) >= limit:
+                tenders = tenders[:limit]
+                break
+
 
         return tenders
-#stopping the automation    
+
+    except (NoSuchElementException, TimeoutError) as e:
+        print(f"Error: {e}")
+        return []
+    
     finally:
         driver.quit()
 
